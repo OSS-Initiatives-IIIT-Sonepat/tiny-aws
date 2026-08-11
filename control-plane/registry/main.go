0d935a56873e639671e29793ea988d2b12b12134
Shubham Singh <shubhsoch@gmail.com>
2026-08-11 18:45:56 +0530

    feat(registry): register end point

--- control-plane/registry/main.go
+++ control-plane/registry/main.go
@@ -13,3 +13,31 @@ type Node struct {
 	Hostname string `json:"hostname"`
 	CPUCount int    `json:"cpu_count"`
 }
+
+func main () {
+	
+	// register end points
+	http.HandleFunc("/nodes", handleNodes)
+
+
+	log.Println("node registry listening on :9000")
+
+	if err := http.ListenAndServe(":9000", nil); err != nil {
+		log.Fatal(err)
+	}
+
+}
+
+func handleNodes(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	if r.Method != http.MethodGet {
+		http.Error(w, "method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	if err := json.NewEncoder(w).Encode(nodes); err != nil {
+		http.Error(w, "failed to encode nodes", http.StatusInternalServerError)
+	}
+
+}

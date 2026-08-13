a21d22d8f4a01fda9c592dc049359c40f3630ab9
Shubham Singh <shubhsoch@gmail.com>
2026-08-13 18:05:12 +0530

    feat(storage): distributed storage system

--- data-plane/storage/storage-node/src/main.rs
+++ data-plane/storage/storage-node/src/main.rs
@@ -1,9 +1,13 @@
 mod block;
 mod store;
+mod server;
 
+use axum::{routing::put, Router};
+use std::sync::Arc;
 use std::path::PathBuf;
 
-fn main() -> std::io::Result<()> {
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
     // call the blockstore constructor (initialize the store)
     let store = store::BlockStore::new(PathBuf::from("data"))?;
 
@@ -18,10 +22,34 @@ fn main() -> std::io::Result<()> {
 
     println!("read: {}", String::from_utf8_lossy(&loaded));
 
-    store.delete_block("block-001")?;
+    // store.delete_block("block-001")?;
 
-    println!("block deleted");
+    println!("block not deleted");
     println!("block written: {}", block.id);
 
+
+    let state = server::AppState {
+        store: Arc::new(store),
+    };
+    
+    use axum::{routing::{delete, get, put}, Router};
+
+    let app = Router::new()
+    .route(
+        "/blocks/{id}",
+        put(server::put_block)
+            .get(server::get_block)
+            .delete(server::delete_block),
+    )
+    .with_state(state);
+    
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:7001").await?;
+    
+    println!("storage-node listening on 127.0.0.1:7001");
+    
+    axum::serve(listener, app)
+        .await
+        .unwrap();
+    
     Ok(())
 }

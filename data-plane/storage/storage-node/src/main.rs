fa28c5d2f8ec0fcc14520c06ae80c84b7845f0fe
Shubham Singh <shubhsoch@gmail.com>
2026-08-13 16:46:16 +0530

    feat(storage): store block

--- data-plane/storage/storage-node/src/main.rs
+++ data-plane/storage/storage-node/src/main.rs
@@ -4,11 +4,17 @@ mod store;
 use std::path::PathBuf;
 
 fn main() -> std::io::Result<()> {
-    // call the blockstore constructor
+    // call the blockstore constructor (initialize the store)
     let store = store::BlockStore::new(PathBuf::from("data"))?;
 
-    println!("storage-node starting...");
-    println!("storage directory initialized");
+    let block = block::Block::new(
+        "block-001".to_string(),
+        b"hello from tiny-aws storage".to_vec(),
+    );
+
+    store.write_block(&block)?;
+
+    println!("block written: {}", block.id);
 
     Ok(())
 }

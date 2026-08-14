8acf35f83e5c152fc58662589995df9f70615400
Shubham Singh <shubhsoch@gmail.com>
2026-08-14 23:00:25 +0530

    feat(storage): add hadoop storage facilitation

--- data-plane/storage/object-store/src/main.rs
+++ data-plane/storage/object-store/src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let client = reqwest::Client::new();
+
+    let response = client
+        .get("http://172.25.48.67:9870/webhdfs/v1/tiny-aws?op=LISTSTATUS")
+        .send()
+        .await?;
+
+    println!("status: {}", response.status());
+    println!("body: {}", response.text().await?);
+
+    Ok(())
 }

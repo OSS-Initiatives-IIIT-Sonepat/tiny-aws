0af391928fe9aadf94383dd4792e585babcac036
Shubham Singh <shubhsoch@gmail.com>
2026-08-28 17:17:09 +0530

    feat(compute): read scheduler url from environment

--- data-plane/compute/ec2-agent/src/config.rs
+++ data-plane/compute/ec2-agent/src/config.rs
@@ -1,3 +1,7 @@
 pub fn registry_url() -> String {
     std::env::var("REGISTRY_URL").unwrap_or_else(|_| "http://127.0.0.1:9000".into())
 }
+
+pub fn scheduler_url() -> String {
+    std::env::var("SCHEDULER_URL").unwrap_or_else(|_| "http://127.0.0.1:9001".into())
+}
